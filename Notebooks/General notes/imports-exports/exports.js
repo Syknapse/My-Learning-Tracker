6e765453d3fec327acf92ef26816cb8bceec8063
Syk Houdeib <syk.houdeib@gmail.com>
2020-09-01 14:59:53 +0200

    update exports

--- Notebooks/General notes/imports-exports/exports.js
+++ Notebooks/General notes/imports-exports/exports.js
@@ -4,5 +4,6 @@ export { variable1 as newName1, variable2 as newName2} // Export under a differe
 
 export { variable1 as default } // Export a specific variable as default
 // Re-export: import to a new file and immediately export
-export { default as Name } from './file.js' // Re-export default
+export { default } from './file.js' // Re-export default
+export { default as Name } from './file.js' // Re-export named default
 export { variable1, variable2 } from './file.js' // Re-export named
